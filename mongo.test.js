dd39ee4b7356e47fe25ac3ada652e6446575c098
Anupam Kumar <akcgjc007@gmail.com>
2021-03-15 19:29:56 +0530

    test working.

--- mongo.test.js
+++ mongo.test.js
@@ -2,32 +2,26 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/test-database`);
 
+// Creating schema
 const {Schema} = mongoose;
-
-const productSchema = new Schema({
-    name: String,
-    age: Number,
-})
-
+const productSchema = new Schema({name: String, age: Number});
 mongoose.model('Students', productSchema);
-
 const Students = mongoose.model('Students');
 
+// Adding and deleting
 async function main(){
-    let name = "Anupam";
-    let age = 21;
+    await Students.deleteMany({});
 
-    await Students.create({
-        name: name,
-        age: age
-    });
+    await Students.create({name: "Anupam", age: 21});
+    await Students.create({name: "Anu", age: 23});
+    await Students.create({name: "Pam", age: 22});
     console.log(await Students.find());
     
-    await Students.deleteOne({name})
+    await Students.deleteOne({name: "Pam"})
     console.log(await Students.find());
 
-    // product = await Students.findByIdAndUpdate(id, req.body);
-    // product = await Students.findByIdAndDelete(id);
+    // await Students.findByIdAndUpdate(id, req.body);
+    // await Students.findByIdAndDelete(id);
 
     return process.exit(1);
 }
