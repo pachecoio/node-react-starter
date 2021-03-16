4473b61ca7bd6de27c97965e41b01f08282f23f5
akcgjc007 <akcgjc007@gmail.com>
2021-03-17 03:55:36 +0530

    commented for deployment testing.

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,49 +7,49 @@ module.exports = (app) => {
     return res.status(200).send([{"status": "OK"}]);
   });
 
-  app.get(`/api/product`, async (req, res) => {
-    let products = await Product.find();
-    return res.status(200).send(products);
-  });
+  // app.get(`/api/product`, async (req, res) => {
+  //   let products = await Product.find();
+  //   return res.status(200).send(products);
+  // });
 
-  app.post(`/api/product/add`, async (req, res) => {
-    let product = await Product.create(req.body);
-    return res.status(201).send({
-      error: false,
-      product
-    })
-  })
+  // app.post(`/api/product/add`, async (req, res) => {
+  //   let product = await Product.create(req.body);
+  //   return res.status(201).send({
+  //     error: false,
+  //     product
+  //   })
+  // })
 
-  app.put(`/api/product/:id`, async (req, res) => {
-    const {id} = req.params;
+  // app.put(`/api/product/:id`, async (req, res) => {
+  //   const {id} = req.params;
 
-    let product = await Product.findByIdAndUpdate(id, req.body);
+  //   let product = await Product.findByIdAndUpdate(id, req.body);
 
-    return res.status(202).send({
-      error: false,
-      product
-    })
+  //   return res.status(202).send({
+  //     error: false,
+  //     product
+  //   })
 
-  });
+  // });
 
-  app.delete(`/api/product/:id`, async (req, res) => {
-    const {id} = req.params;
+  // app.delete(`/api/product/:id`, async (req, res) => {
+  //   const {id} = req.params;
 
-    let product = await Product.findByIdAndDelete(id);
+  //   let product = await Product.findByIdAndDelete(id);
 
-    return res.status(202).send({
-      error: false,
-      product
-    })
+  //   return res.status(202).send({
+  //     error: false,
+  //     product
+  //   })
 
-  });
+  // });
 
-  app.post(`/api/product/remove`, async(req, res)=>{
-    let l = await Product.remove({name: req.body.name})
-    console.log(req.body.name);
-    return res.status(202).send({
-      error: false,
-      l
-    });
-  });
+  // app.post(`/api/product/remove`, async(req, res)=>{
+  //   let l = await Product.remove({name: req.body.name})
+  //   console.log(req.body.name);
+  //   return res.status(202).send({
+  //     error: false,
+  //     l
+  //   });
+  // });
 };
